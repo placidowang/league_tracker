40ac1a6adc694df096b245b729916cb4bde185a8
placidowang <placidow@gmail.com>
2020-04-10 16:50:17 -0500

    finalized changes with master

--- frontend/src/components/ChampionCard.js
+++ frontend/src/components/ChampionCard.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Card, Image } from 'semantic-ui-react'
 
 const ChampionCard = (props) => {
   return(
     <Grid.Column>
+      <Card>
       <Image src={props.champion.icon_image} />
       {`${props.champion.name}, ${props.champion.title}`}
+      </Card>
     </Grid.Column>
   )
 }
