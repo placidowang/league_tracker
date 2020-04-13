c93768b6016161e685d23a02f1cc6f125364a847
vienpham2019 <vienpham2019@gmail.com>
2020-04-13 10:26:01 -0500

    save

--- frontend/src/components/ChampionInfo.js
+++ frontend/src/components/ChampionInfo.js
@@ -10,7 +10,7 @@ export default class ChampionInfo extends Component {
 
     componentDidUpdate(prevProps){
         if(prevProps.championId !== this.props.championId){
-            fetch(` http://localhost:3000/champions/${131}`)
+            fetch(`http://localhost:3000/champions/${146}`)
             .then(res => res.json())
             .then(champion => this.setState({champion}))
         }
