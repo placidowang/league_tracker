6c070712f9af284bb064af175c8b8169e4353a0e
vienpham2019 <vienpham2019@gmail.com>
2020-04-14 15:20:04 -0500

    save

--- backend/app/controllers/summoner_profiles_controller.rb
+++ backend/app/controllers/summoner_profiles_controller.rb
@@ -8,11 +8,16 @@ class SummonerProfilesController < ApplicationController
 
     def search_summoner
         puts "Hello"
-        url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/PureAlchemy?api_key=RGAPI-0dc4405e-38d9-4bf3-8b9a-17f82620ec5d"
+        # url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/PureAlchemy?api_key=RGAPI-0dc4405e-38d9-4bf3-8b9a-17f82620ec5d"
+        # response = HTTParty.get(url)
+        # summoner = response.parsed_response
+        # # byebug
+        # render json: response
+
+        url = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/bexzz?api_key=RGAPI-9e67cfe9-579d-40fe-b050-2214431835af"
         response = HTTParty.get(url)
         summoner = response.parsed_response
-        # byebug
-        render json: response
+        render json: summoner
     end
 
 end
