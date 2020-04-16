1766f5d8f1bdba466ddb881ef20225dd2dacd0b6
placidowang <placidow@gmail.com>
2020-04-16 10:21:33 -0500

    navbar styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -11,22 +11,20 @@ export default class NavBar extends React.Component {
 
       <header className="header-nav">
         <nav className="navbar">
-          <div>
-            <SideMenuToggle />
-          </div>
-          <div className="navbar-logo"><a href="/">The Logo</a></div>
+          {/* <div><SideMenuToggle /></div> */}
+          <div className="navbar-logo"><a href="/"><img src="./images/LT-logo.png" alt="LT logo"/></a></div>
           <div className="spacer" />
           <div className="navbar-navigation-items">
             <ul>
               <li>
                 <button onClick = {() => {
                   window.scrollTo(0, 0)
-                }}>
+                }} className="navbar-button">
                   <NavLink to = "/summoner">Summoner</NavLink>
                 </button>
               </li>
               <li>
-                <button onClick = {() => window.scrollTo(0, 0)}>
+                <button onClick = {() => window.scrollTo(0, 0)} className="navbar-button">
                   <NavLink to = "/champions">Champions</NavLink>
                 </button>
               </li>  
@@ -43,7 +41,7 @@ export default class NavBar extends React.Component {
                       localStorage.clear()
                       this.props.checkForLogin()
                       this.props.displayMessage("LogOut Success")
-                    }}>
+                    }} className="navbar-button">
                       LogOut
                    </button>
                   </li>
@@ -52,7 +50,7 @@ export default class NavBar extends React.Component {
                     <button onClick= {() => {
                       window.scrollTo(0, 0)
                       localStorage.clear()
-                    }}>
+                    }} className="navbar-button">
                       <NavLink to = "/login">Login</NavLink>
                     </button>
                   </li>
